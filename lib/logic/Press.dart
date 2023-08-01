37844e7240537a0493f7a633b0e99dab3c34b55a
sajadrajaei2000 <sajadrajaei2000@gmail.com>
2023-08-01 09:37:44 -0400

    commit 2

---
+++ lib/logic/Press.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: avoid_print
+
+class Press {
+  onUpPressed() {
+    print('Up');
+  }
+
+  onDownPressed() {
+    print('Down');
+  }
+
+  onRightPressed() {
+    print('Right');
+  }
+
+  onLeftPressed() {
+    print('Left');
+  }
+
+  onCrossPressed() {
+    print('Cross');
+  }
+
+  onCirclePressed() {
+    print('Circle');
+  }
+
+  onSquarePressed() {
+    print('Square');
+  }
+
+  onTrianglePressed() {
+    print('Triangle');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:project4/logic/Press.dart';
 
 void main(List<String> args) {
   SystemChrome.setPreferredOrientations([
@@ -15,9 +16,15 @@ void main(List<String> args) {
   ));
 }
 
-class Remote_Car_Control extends StatelessWidget {
+class Remote_Car_Control extends StatefulWidget {
   const Remote_Car_Control({super.key});
 
+  @override
+  State<Remote_Car_Control> createState() => _Remote_Car_ControlState();
+}
+
+class _Remote_Car_ControlState extends State<Remote_Car_Control> {
+  final _press = Press();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,76 +37,100 @@ class Remote_Car_Control extends StatelessWidget {
                 right: 70,
                 bottom: -50,
                 top: 10,
-                child: SvgPicture.asset(
-                  'assets/up.svg',
-                  width: 40,
-                  height: 60,
+                child: GestureDetector(
+                  onTap: _press.onUpPressed,
+                  child: SvgPicture.asset(
+                    'assets/up.svg',
+                    width: 40,
+                    height: 60,
+                  ),
                 ),
               ),
               Positioned(
                 right: 15,
                 bottom: -150,
                 top: 0,
-                child: SvgPicture.asset(
-                  'assets/right.svg',
-                  width: 30,
-                  height: 50,
+                child: GestureDetector(
+                  onTap: _press.onRightPressed,
+                  child: SvgPicture.asset(
+                    'assets/right.svg',
+                    width: 30,
+                    height: 50,
+                  ),
                 ),
               ),
               Positioned(
                 right: 70,
                 bottom: -250,
                 top: -10,
-                child: SvgPicture.asset(
-                  'assets/bottom.svg',
-                  width: 40,
-                  height: 60,
+                child: GestureDetector(
+                  onTap: _press.onDownPressed,
+                  child: SvgPicture.asset(
+                    'assets/bottom.svg',
+                    width: 40,
+                    height: 60,
+                  ),
                 ),
               ),
               Positioned(
                 right: 115,
                 bottom: -150,
                 top: 0,
-                child: SvgPicture.asset(
-                  'assets/left.svg',
-                  width: 30,
-                  height: 50,
+                child: GestureDetector(
+                  onTap: _press.onLeftPressed,
+                  child: SvgPicture.asset(
+                    'assets/left.svg',
+                    width: 30,
+                    height: 50,
+                  ),
                 ),
               ),
               Positioned(
                 left: 80,
                 bottom: 120,
-                child: SvgPicture.asset(
-                  'assets/PS4_Triangle.svg',
-                  width: 40,
-                  height: 60,
+                child: GestureDetector(
+                  onTap: _press.onTrianglePressed,
+                  child: SvgPicture.asset(
+                    'assets/PS4_Triangle.svg',
+                    width: 40,
+                    height: 60,
+                  ),
                 ),
               ),
               Positioned(
                 left: 135,
                 bottom: 80,
-                child: SvgPicture.asset(
-                  'assets/PS4_Circle.svg',
-                  width: 40,
-                  height: 60,
+                child: GestureDetector(
+                  onTap: _press.onCirclePressed,
+                  child: SvgPicture.asset(
+                    'assets/PS4_Circle.svg',
+                    width: 40,
+                    height: 60,
+                  ),
                 ),
               ),
               Positioned(
                 left: 80,
                 bottom: 35,
-                child: SvgPicture.asset(
-                  'assets/PS4_Cross.svg',
-                  width: 40,
-                  height: 60,
+                child: GestureDetector(
+                  onTap: _press.onCrossPressed,
+                  child: SvgPicture.asset(
+                    'assets/PS4_Cross.svg',
+                    width: 40,
+                    height: 60,
+                  ),
                 ),
               ),
               Positioned(
                 left: 25,
                 bottom: 80,
-                child: SvgPicture.asset(
-                  'assets/PS4_Square.svg',
-                  width: 40,
-                  height: 60,
+                child: GestureDetector(
+                  onTap: _press.onSquarePressed,
+                  child: SvgPicture.asset(
+                    'assets/PS4_Square.svg',
+                    width: 40,
+                    height: 60,
+                  ),
                 ),
               ),
               // Add more Positioned widgets with other SVGs as needed
@@ -108,5 +139,9 @@ class Remote_Car_Control extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  onUpPressed() {
+    print('Up');
   }
 }
